9a985f0708f12cba7961d3f4ac8fda72e5b75671
David Stansby <dstansby@gmail.com>
2025-04-03 22:12:50 +0100

    Don't allow None filters

--- src/pydantic_zarr/v2.py
+++ src/pydantic_zarr/v2.py
@@ -201,7 +201,7 @@ class ArraySpec(NodeSpec, Generic[TAttr]):
         attributes: Literal["auto"] | TAttr = "auto",
         fill_value: Literal["auto"] | float | None = "auto",
         order: Literal["auto", "C", "F"] = "auto",
-        filters: Literal["auto"] | list[CodecDict] | None = "auto",
+        filters: Literal["auto"] | list[CodecDict] = "auto",
         dimension_separator: Literal["auto", "/", "."] = "auto",
         compressor: Literal["auto"] | CodecDict | None = "auto",
     ) -> Self:
@@ -230,7 +230,7 @@ class ArraySpec(NodeSpec, Generic[TAttr]):
             The fill value for this array. Either "auto" or FillValue. The default is "auto", which means that `array.fill_value` will be used if that attribute exists, with a fallback value of 0.
         compressor: "auto" | CodecDict | None, default = "auto"
             The compressor for this `ArraySpec`. One of "auto", a JSON-serializable representation of a compression codec, or `None`. The default is "auto", which means that `array.compressor` attribute will be used, with a fallback value of `None`.
-        filters: "auto" | List[CodecDict] | None, default = "auto"
+        filters: "auto" | List[CodecDict], default = "auto"
             The filters for this `ArraySpec`. One of "auto", a list of JSON-serializable representations of compression codec, or `None`. The default is "auto", which means that the `array.filters` attribute will be
             used, with a fallback value of `None`.
         dimension_separator: "auto" | "." | "/", default = "auto"
@@ -324,7 +324,6 @@ class ArraySpec(NodeSpec, Generic[TAttr]):
         ArraySpec(zarr_version=2, attributes={}, shape=(10, 10), chunks=(10, 10), dtype='<f8', fill_value=0.0, order='C', filters=None, dimension_separator='.', compressor={'id': 'blosc', 'cname': 'lz4', 'clevel': 5, 'shuffle': 1, 'blocksize': 0})
 
         """
-        filters = array.filters if len(array.filters) else None
         return cls(
             shape=array.shape,
             chunks=array.chunks,
@@ -333,7 +332,7 @@ class ArraySpec(NodeSpec, Generic[TAttr]):
             # so that int 0 isn't serialized as 0.0
             fill_value=array.dtype.type(array.fill_value).tolist(),
             order=array.order,
-            filters=filters,
+            filters=array.filters,
             dimension_separator=array.metadata.dimension_separator,
             compressor=array.compressors[0].get_config(),
             attributes=array.attrs.asdict(),
@@ -893,7 +892,7 @@ def from_flat(data: dict[str, ArraySpec | GroupSpec]) -> ArraySpec | GroupSpec:
     >>> import numpy as np
     >>> tree = {'': ArraySpec.from_array(np.arange(10))}
     >>> from_flat(tree) # special case of a Zarr array at the root of the hierarchy
-    ArraySpec(zarr_version=2, attributes={}, shape=(10,), chunks=(10,), dtype='<i8', fill_value=0, order='C', filters=None, dimension_separator='/', compressor=None)
+    ArraySpec(zarr_version=2, attributes={}, shape=(10,), chunks=(10,), dtype='<i8', fill_value=0, order='C', filters=[], dimension_separator='/', compressor=None)
     >>> tree = {'/foo': ArraySpec.from_array(np.arange(10))}
     >>> from_flat(tree) # note that an implicit Group is created
     GroupSpec(zarr_version=2, attributes={}, members={'foo': ArraySpec(zarr_version=2, attributes={}, shape=(10,), chunks=(10,), dtype='<i8', fill_value=0, order='C', filters=None, dimension_separator='/', compressor=None)})
